dd6366dd6d36155a30b1566ef2295abcebeb16d6
dudifreund <dudifreund@gmail.com>
2020-11-27 21:50:19 +0000

    Added player and implemented movement

---
+++ Clothes Shop/Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+
+    private Vector2 currentMovement;
+    private bool isWalking = false;
+    
+    private void Update()
+    {
+        GetInput();
+    }
+    
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        currentMovement.x = Input.GetAxisRaw("Horizontal");
+        currentMovement.y = Input.GetAxisRaw("Vertical");
+
+        // Prevent diagonal movement
+        if (currentMovement.x != 0)
+        {
+            currentMovement.y = 0f;
+        }
+        
+        if(currentMovement.x != 0f || currentMovement.y != 0f)
+        {
+            if (!isWalking)
+            {
+                isWalking = true;
+                animator.SetBool("isWalking", isWalking);
+            }
+
+            animator.SetFloat("Xmovement", currentMovement.x);
+            animator.SetFloat("Ymovement", currentMovement.y);
+        }
+        else
+        {
+            if (isWalking)
+            {
+                isWalking = false;
+                animator.SetBool("isWalking", isWalking);
+            }
+        }
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + currentMovement * movementSpeed * Time.fixedDeltaTime); 
+    }
+}
